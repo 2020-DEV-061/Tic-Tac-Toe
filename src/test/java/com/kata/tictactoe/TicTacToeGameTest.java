6b5ee1c4c26be6d7c5b0e51376541a8ea89ced91
2020-DEV-061 <62551578+2020-DEV-061@users.noreply.github.com>
2020-03-24 12:15:53 +0100

    Refactor: Formatting indentation of testcases and removing unused initialization of TicTacToeGame object.

--- src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
+++ src/test/java/com/kata/tictactoe/TicTacToeGameTest.java
@@ -16,16 +16,16 @@ public class TicTacToeGameTest {
 
     @Test
     public void getPlayerAtShouldReturnXWhenFirstTurnIsPlayed() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         ticTacToeGame.playTurnAt(0, 0);
+
         assertEquals('X', ticTacToeGame.getPlayerAt(0, 0));
     }
 
     @Test
     public void getPlayerAtShouldReturnOWhenSecondTurnIsPlayed() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
         ticTacToeGame.playTurnAt(0, 0);
         ticTacToeGame.playTurnAt(0, 1);
+
         assertEquals('O', ticTacToeGame.getPlayerAt(0, 1));
     }
 
@@ -34,6 +34,7 @@ public class TicTacToeGameTest {
         ticTacToeGame.playTurnAt(0, 0);
         ticTacToeGame.playTurnAt(0, 1);
         ticTacToeGame.playTurnAt(0, 2);
+
         assertEquals('X', ticTacToeGame.getPlayerAt(0, 2));
     }
 
@@ -45,167 +46,184 @@ public class TicTacToeGameTest {
 
     @Test
     public void playTurnAtShouldReturnXIsWinnerWhenEntireFirstRowIsOccupiedByX() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(1,0);
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(2,0);
-        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(0,2));
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(1, 0);
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(2, 0);
+
+        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(0, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnXIsWinnerWhenEntireSecondRowIsOccupiedByX() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(1,0);
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(2,0);
-        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(1,2));
+        ticTacToeGame.playTurnAt(1, 0);
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(2, 0);
+
+        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(1, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnXIsWinnerWhenEntireThirdRowIsOccupiedByX() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(2,0);
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(2,1);
-        ticTacToeGame.playTurnAt(1,0);
-        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2,2));
+        ticTacToeGame.playTurnAt(2, 0);
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(2, 1);
+        ticTacToeGame.playTurnAt(1, 0);
+
+        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnOIsWinnerWhenEntireFirstRowIsOccupiedByO() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(1,0);
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(2,0);
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(2,2);
-        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(0,2));
+        ticTacToeGame.playTurnAt(1, 0);
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(2, 0);
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(2, 2);
+
+        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(0, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnOIsWinnerWhenEntireSecondRowIsOccupiedByO() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(1,0);
-        ticTacToeGame.playTurnAt(2,0);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(2,2);
-        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(1,2));
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(1, 0);
+        ticTacToeGame.playTurnAt(2, 0);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(2, 2);
+
+        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(1, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnOIsWinnerWhenEntireThirdRowIsOccupiedByO() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(2,0);
-        ticTacToeGame.playTurnAt(1,0);
-        ticTacToeGame.playTurnAt(2,1);
-        ticTacToeGame.playTurnAt(0,2);
-        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2,2));
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(2, 0);
+        ticTacToeGame.playTurnAt(1, 0);
+        ticTacToeGame.playTurnAt(2, 1);
+        ticTacToeGame.playTurnAt(0, 2);
+
+        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnXIsWinnerWhenEntireFirstColumnIsOccupiedByX() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(1,0);
-        ticTacToeGame.playTurnAt(0,2);
-        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2,0));
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(1, 0);
+        ticTacToeGame.playTurnAt(0, 2);
+
+        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2, 0));
     }
 
     @Test
     public void playTurnAtShouldReturnXIsWinnerWhenEntireSecondColumnIsOccupiedByX() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(0,2);
-        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2,1));
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(0, 2);
+
+        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2, 1));
     }
 
     @Test
     public void playTurnAtShouldReturnXIsWinnerWhenEntireThirdColumnIsOccupiedByX() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,2);
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(1,2);
-        ticTacToeGame.playTurnAt(0,1);
-        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2,2));
+        ticTacToeGame.playTurnAt(0, 2);
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(1, 2);
+        ticTacToeGame.playTurnAt(0, 1);
+
+        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnOIsWinnerWhenEntireFirstColumnIsOccupiedByO() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(1,2);
-        ticTacToeGame.playTurnAt(1,0);
-        ticTacToeGame.playTurnAt(2,2);
-        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2,0));
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(1, 2);
+        ticTacToeGame.playTurnAt(1, 0);
+        ticTacToeGame.playTurnAt(2, 2);
+
+        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2, 0));
     }
 
     @Test
     public void playTurnAtShouldReturnOIsWinnerWhenEntireSecondColumnIsOccupiedByO() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(1,2);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(2,0);
-        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2,1));
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(1, 2);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(2, 0);
+
+        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2, 1));
     }
 
     @Test
     public void playTurnAtShouldReturnOIsWinnerWhenEntireThirdColumnIsOccupiedByO() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(0,2);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(1,2);
-        ticTacToeGame.playTurnAt(2,0);
-        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2,2));
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(0, 2);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(1, 2);
+        ticTacToeGame.playTurnAt(2, 0);
+
+        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnXIsWinnerWhenEntireLeftToRightDiagonalIsOccupiedByX() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(0,2);
-        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2,2));
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(0, 2);
+
+        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnOIsWinnerWhenEntireLeftToRightDiagonalIsOccupiedByO() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(0,2);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(1,2);
-        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2,2));
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(0, 2);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(1, 2);
+
+        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2, 2));
     }
 
     @Test
     public void playTurnAtShouldReturnXIsWinnerWhenEntireRightToLeftDiagonalIsOccupiedByX() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,2);
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(0,0);
-        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2,0));
+        ticTacToeGame.playTurnAt(0, 2);
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(0, 0);
+
+        assertEquals("Player X is the Winner", ticTacToeGame.playTurnAt(2, 0));
     }
 
     @Test
     public void playTurnAtShouldReturnOIsWinnerWhenEntireRightToLeftDiagonalIsOccupiedByO() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(0,2);
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(1,0);
-        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2,0));
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(0, 2);
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(1, 0);
+
+        assertEquals("Player O is the Winner", ticTacToeGame.playTurnAt(2, 0));
     }
 
     @Test
     public void playTurnAtShouldReturnItsADrawWhenNineChancesArePlayedAndNeitherPlayerHas3InARow() throws PositionAlreadyInUseException, PositionOutOfGridException {
-        ticTacToeGame.playTurnAt(0,0);
-        ticTacToeGame.playTurnAt(0,2);
-        ticTacToeGame.playTurnAt(0,1);
-        ticTacToeGame.playTurnAt(1,0);
-        ticTacToeGame.playTurnAt(1,2);
-        ticTacToeGame.playTurnAt(1,1);
-        ticTacToeGame.playTurnAt(2,0);
-        ticTacToeGame.playTurnAt(2,1);
-        assertEquals("It is a Draw", ticTacToeGame.playTurnAt(2,2));
+        ticTacToeGame.playTurnAt(0, 0);
+        ticTacToeGame.playTurnAt(0, 2);
+        ticTacToeGame.playTurnAt(0, 1);
+        ticTacToeGame.playTurnAt(1, 0);
+        ticTacToeGame.playTurnAt(1, 2);
+        ticTacToeGame.playTurnAt(1, 1);
+        ticTacToeGame.playTurnAt(2, 0);
+        ticTacToeGame.playTurnAt(2, 1);
+
+        assertEquals("It is a Draw", ticTacToeGame.playTurnAt(2, 2));
     }
 
     @Test(expected = PositionOutOfGridException.class)
